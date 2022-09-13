0ede69d075fc9fdfc47a4cf35d43f15d7bcdf9c7
Mayantra <isaac.vassquez@gmail.com>
2022-09-12 18:56:33 -0600

    Templates arreglados

--- tacanaproject/apps/home/urls.py
+++ tacanaproject/apps/home/urls.py
@@ -6,5 +6,7 @@ app_name = 'home'
 urlpatterns =[
     path('index/', indexView.as_view(), name='index'),
     path('proyectosA/', proyectosView.as_view(), name='proyectosA'),
+    path('detallesproject/', detallesProject.as_view(), name='detallesproject'),
+    path('contacto/', contactoView.as_view(), name='contacto'),
     path('ver/', verView.as_view(), name='ver'),
 ]
--- tacanaproject/apps/home/views.py
+++ tacanaproject/apps/home/views.py
@@ -4,11 +4,21 @@ from django.views.generic import *
 
 # Create your views here.
 
+#Se encuentra el principal de la página
 class indexView (TemplateView):
     template_name = 'index.html'
 
+#Encotramos el apartado donde estarán todos los proyectos
 class proyectosView(TemplateView):
     template_name = 'proyectosA.html'
+
+#apartado donde estaran la descripción especifica de cada proyecto
+class detallesProject(TemplateView):
+    template_name = 'detallesProject.html'
+
+#Pagina para ponerse en contacto
+class contactoView(TemplateView):
+    template_name = 'contact.html'
 
 class verView(TemplateView):
     template_name = 'service.html'
